60eb8d63b131f7c50ff34d0e794797f2bb6bd349
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-06 22:26:10 +0000

    Create a function for compressing a folder into a tar file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let output_filename = "archive.tar.gz";
+    let src_directory = "midjourney";
+    let output_path = ".";
+    compress_folder(&output_filename, &src_directory, &output_path).unwrap();
+}
+
+fn compress_folder(
+    output_filename: &str,
+    src_directory: &str,
+    output_path: &str,
+) -> Result<(), std::io::Error> {
+    // create an empty tar file
+    let tar_gz = File::create(output_filename).unwrap();
+    // write midjourney forlder contents into the tar file
+    let tar_file = GzEncoder::new(tar_gz, Compression::default());
+    let mut tar = tar::Builder::new(tar_file);
+    tar.append_dir_all(output_path, src_directory).unwrap();
+    Ok(())
 }
